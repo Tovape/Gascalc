8d7e444ffd03a7533570ae5ae237c5f7f4e0482a
Tovape <tonivalverde0@gmail.com>
2023-03-06 11:44:20 +0100

    Para instalar el node modules

---
+++ dependencias.bat
@@ -0,0 +1,1 @@
+npm i bcrypt body-parser connect-busboy crypto-js ejs express express-flash express-session forever fs-extra method-override mysql nodemailer node-fetch passport passport-local serve-favicon
