a91ad5e504d0fd1478cf69d900dcc62fef4f588e
Tovape <tonivalverde0@gmail.com>
2023-03-09 12:08:35 +0100

    cambiamos a php

--- js/password-javascript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-	var url = new URL(document.location);
-	var ref = url.searchParams.get("ref");	
-	console.log(ref)
-	document.getElementById("encrypted").setAttribute('value',ref);
-
-});
